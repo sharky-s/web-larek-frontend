c3af2e25cc251398a6dbeb54a5442eb79b4a443f
sharky-s <yulya.sharkova.2002@gmail.com>
2025-07-20 17:19:00 +0300

    :memo:

--- src/types/index.ts
+++ src/types/index.ts
@@ -24,22 +24,31 @@ export interface IProductsData {
   products: IProduct[]; // массив товаров
   preview: string | null; // выбранная карточка товара
   getProduct(id: string): IProduct | undefined; // метод для получения товара по ID
+  getProducts() : IProduct[];
+  setProducts(products: IProduct[]) : void;
+  setPreview(product: IProduct) : void;
 }
 
 export interface ICartData {
-  items: IProduct[]; // массив товаров в корзине
-  total: number; // общая сумма товаров в корзине
+  items: TCartItem[]; // массив товаров в корзине
   addItem(product: IProduct): void; // метод для добавления товара в корзину
   removeItem(id: string): void; // метод для удаления товара из корзины
   clear(): void; // метод для очистки корзины
   checkProduct(id: string): boolean; // метод для проверки наличия товаров в корзине
   getItemsId(): string[]; // метод для получения всех товаров в корзине
+  getItemsCount() : number; // метод для получения количества товаров в корзине
+  getItemsTotal() : number; // метод дял получения общей стоимости товаров в корзине
 }
 
 export interface IBuyersData{
-  setBuyerData(data: IBuyer): void; // метод для установки данных покупателя
-  chekPaymentValidation(data: Record<keyof TPaymentForm, string>): boolean; // метод для проверки валидности формы оплаты
+  order: IBuyer; // данные заказа
+  //errors: МОЖЕТ ПОНАДОБИТЬСЯ
+  setPaymentData(data: Record<keyof TPaymentForm, string>) : void;
+  setContactData(data: Record<keyof TContactForm, string>) : void;
+  chekPaymentValidation(data: Record<keyof TPaymentForm, string>): 
+  boolean; // метод для проверки валидности формы оплаты
   chekContactValidation(data: Record<keyof TContactForm, string>): boolean; // метод для проверки валидности формы контактов
+  clear() : void; // метод очистки данных заказа
 }
 
 export type TCartItem = Pick<IProduct, 'id' | 'title' | 'price'>;
